27c4fbb1af38732d665c2e00874030f547bf108d
Muhammad A Faishal <maafaishal@gmail.com>
2025-02-21 13:54:43 +0700

    build: vercel vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: process.env.PORT as unknown as number,
   },
 });
